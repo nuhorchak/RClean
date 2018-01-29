93f2ceaf85810b458b3c8cdce9dd65b975b74bf6
nuhorchak <nuhorchak@gmail.com>
2018-01-29 09:08:12 -0500

    edit description - still has issues

---
+++ R/run_my_app.R
@@ -0,0 +1,5 @@
+run_my_app <- function(dir){
+  app_dir <- dirname('C:\\Users\\nuhorchak\\Desktop\\OPER782\\Uhorchak_Analytic\\RClean\\inst\\apps\\my_first_app\\us.R')
+  
+  shiny::shinyAppDir(app_dir)
+}
