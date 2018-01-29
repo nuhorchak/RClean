76e5b67552997b90663259607e2133230dcbd8fe
nuhorchak <nuhorchak@gmail.com>
2018-01-29 08:21:25 -0500

    update to APP

---
+++ inst/apps/my_first_app/server.R
@@ -0,0 +1,15 @@
+server <- function(input, output, session){
+  
+  output$map <- renderPlot({
+    map_data$value = map_data[,input$select]
+    
+    state_choropleth(df=map_data,
+                     title = colnames(map_data)[input$select],
+                     num_colors = input$num_colors)
+  })
+  
+  output$table <- renderDataTable({
+    map_data[order(map_data[input$select]),]
+  })
+  
+}
