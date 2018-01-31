c1982a8cc34be76bdecb3fafdacea2b93a814c2e
nuhorchak <nuhorchak@gmail.com>
2018-01-31 08:07:39 -0500

    fix app_dir pointer location

--- R/run_my_app.R
+++ R/run_my_app.R
@@ -1,5 +1,5 @@
 run_my_app <- function(dir){
-  app_dir <- dirname('C:\\Users\\nuhorchak\\Desktop\\OPER782\\Uhorchak_Analytic\\RClean\\inst\\apps\\my_first_app\\us.R')
+  app_dir <- dirname('C:\\Users\\nuhorchak\\Desktop\\OPER782\\Uhorchak_Analytic\\RClean\\inst\\apps\\my_first_app\\server.R')
   
   shiny::shinyAppDir(app_dir)
 }
