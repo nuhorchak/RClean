bac4ac2857b546f5aa961fc97959fedd1a9be600
nuhorchak <nuhorchak@gmail.com>
2018-02-05 08:59:17 -0500

    5FEB updates

---
+++ R/data.R
@@ -0,0 +1,16 @@
+
+#' @title Firsher's Iris dataset
+#'
+#' @description
+#' A dataset containing a . Dataset taken from kaggle, in .xls format, from 
+#' https://www.kaggle.com/saurabh00007/iriscsv
+#'
+#' @format A \code{tibble} with 200 rows and 3 variables
+#'
+#' \describe{
+#'   \item{PSC}{Contract identifier for each product and service}
+#'   \item{x_attribute}{x attribute score, from 1 (worst) to 5 (best) in increments of 0.01}
+#'   \item{y_attribute}{y attribute score, from 1 (worst) to 10 (best) in increments of 0.01}
+#' }
+#'
+'iris'
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(RClean)
+
+test_check("RClean")
