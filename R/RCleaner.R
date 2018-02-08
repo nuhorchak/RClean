b7441089821b90dff23178671bf4edaa04db3b6e
nuhorchak <nuhorchak@gmail.com>
2018-02-07 20:04:43 -0500

    working

--- R/RCleaner.R
+++ R/RCleaner.R
@@ -21,7 +21,8 @@ RCleaner <- function(data, ...) {
     miniContentPanel(
       DT::dataTableOutput("Main_table")
     ),
-    miniButtonBlock(actionButton("deleteRows", "Delete Rows"), actionButton("deleteCols", "Delete Cols"))
+    miniButtonBlock(actionButton("deleteRows", "Delete Rows"), actionButton("deleteCols", "Delete Cols")),
+    theme = 'darkly'
   )
 
   server <- function(input, output, session) {
@@ -48,14 +49,24 @@ RCleaner <- function(data, ...) {
                                              server = TRUE, 
                                              selection = list(target = 'row+column'))
 
+    #BUTTON LOGIC
     # Handle the Done button being pressed.
     observeEvent(input$done, {
       # Return the modified datatable...not working yet
-    stopApp(my_data <<- data.frame(values$dfWorking))
+    stopApp(clean_data <<- data.frame(values$dfWorking))
+    #stopApp(list(my_data = data.frame(values$dfWorking)))
     })
+    
+    #cancel logic - for some reason it stopped working
+    observeEvent(input$cancel, {
+      stopApp(print("User cancelled action"))
+    })
+    
+    
   }
 
   shinygadgets::runGadget(ui, server, viewer = shinygadgets::dialogViewer("RCleaner"))
   
+  #code to select and remove rows was modified from the following:
   #https://stackoverflow.com/questions/39136385/delete-row-of-dt-data-table-in-shiny-app
 }
